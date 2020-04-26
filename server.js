93f4fe05f62c4f5946acf1d6391b17df91d84c11
rajnish76 <rajnishsrivastava>
2020-04-26 12:55:51 +0530

    Initial express setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Running on ${PORT}`));
